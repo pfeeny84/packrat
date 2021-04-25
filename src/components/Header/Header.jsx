27c4a179253859b65fa455c34d0274ef1d921052
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-04-25 13:12:42 -0700

    added custom icon to header home link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { Header, Segment, Image, Icon } from 'semantic-ui-react';
+import { FaGuitar } from 'react-icons/fa';
 
 
 export default function PageHeader({user, handleLogout}){
     return (
         <Segment clearing>
             <Header as='h2' floated='right'>
-                <Link to="/"><Icon name="home"></Icon></Link>
+                <Link to="/"><FaGuitar /></Link>
                 <Link to='' onClick={handleLogout}>Logout</Link>
             </Header>
             <Header as='h2' floated='left'>
-                <Link to={`/${user.username}`}><Image src={user.photoUrl ? user.photoUrl : "https://react.semantic-ui.com/images/wireframe/square-image.png"} avatar></Image></Link>          
+                <Link to={`/${user.username}`}><Image src={user.photoUrl ? user.photoUrl : "https://react.semantic-ui.com/images/wireframe/square-image.png"} avatar></Image>{user.username}</Link>          
             </Header>
         </Segment>
     )
+    
 }

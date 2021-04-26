0201a7a19f521ec980ab7fda38825dbf0d03d029
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-04-26 16:33:19 -0700

    linking the post user avatar to be clickable and route to the users profile page that made the post.

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -24,13 +24,13 @@ function PostCard({post, isProfile, addLike, removeLike, user }) {
      {isProfile ? ''
         :  
           <Card.Content textAlign='left'>
-              <Image
+              <Link style={{color: 'red'}} to={`/${post.user.username}`}><Image
                   floated='left'
                   size='huge'
                   avatar
                   src={post.user.photoUrl ? post.user.photoUrl : 'https://react.semantic-ui.com/images/wireframe/square-image.png'}
-              />
-              <Card.Header  floated="right">{post.user.username}</Card.Header>
+              /></Link>
+              <Card.Header  floated="right">{post.user.username.toUpperCase()}</Card.Header>
           </Card.Content>
       
       }

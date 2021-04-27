0da186b43231265b7a89b56b31c56c14ca463604
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-04-27 11:40:41 -0700

    implementing profile page and post feed. Need to set up bio

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,38 +1,107 @@
 import React, { useState, useEffect } from 'react';
-import {  Grid } from 'semantic-ui-react';
-import ProfileBio from '../../components/ProfileBio/ProfileBio';
-import ProfilePostDisplay from '../../components/ProfilePostDisplay/ProfilePostDisplay';
-import PageHeader from '../../components/Header/Header';
+import { Grid, Segment, Dimmer, Loader } from 'semantic-ui-react'
 import userService from '../../utils/userService';
+import ProfileBio from '../../components/ProfileBio/ProfileBio';
+import PostFeed from '../../components/PostFeed/PostFeed';
+import PageHeader from '../../components/Header/Header';
+import * as likesApi from '../../utils/likesService';
 import { useLocation } from 'react-router-dom';
 
+export default function ProfilePage({ user, handleLogout }) {
+
+    const [posts, setPosts] = useState([])
+    const [profileUser, setProfileUser] = useState({})
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+
+    const location = useLocation()
+    console.log(location)
 
 
-export default function ProfilePage({ user, handleLogout }){
+    async function addLike(postId){
+        try {
+          const data = await likesApi.create(postId)
+          console.log(data, ' response from addLike')
+          getProfile()
+        } catch(err){
+          console.log(err)
+        }
+      }
+  
+      async function removeLike(likeId){
+        try{  
+          const data = await likesApi.removeLike(likeId);
+          console.log(data, ' response from removeLike')
+          getProfile()
+        } catch(err){
+          console.log(err)
+        }
+      }
+
+
+
+
+    async function getProfile() {
+
+        try {
+
+            const username = location.pathname.substring(1)
+            // location.pathname returns /jimbo so we need to cut off the / using the js method substring
+            // This gets the username from the url! 
+            console.log(username)
+            const data = await userService.getProfile(username);
+            console.log(data)
+            setLoading(() => false)
+            setPosts(() => [...data.posts])
+            setProfileUser(() => data.user)
+        } catch (err) {
+            console.log(err)
+            setError(err)
+        }
+    }
+
+
     
+
+    useEffect(() => {
+        getProfile()
+
+    }, [])
+
+
 
     return (
+
         <>
-        <Grid>
-        <Grid.Row>
-            <Grid.Column>
-                <PageHeader user={user} handleLogout={handleLogout}/>
-            </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-            <Grid.Column>
-                <ProfileBio />
-            </Grid.Column>
-        </Grid.Row>
-        <Grid.Row centered>
-            <Grid.Column style={{ maxWidth: 750 }}>
-                <ProfilePostDisplay />
-            </Grid.Column>
-            </Grid.Row>
-        </Grid>
-          
-             <h1>This is the Profile Page</h1>
-    
-      </>
-      );
+            { loading ?
+                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle' >
+                
+                        <Grid.Column style={{ maxWidth: 450}}>
+                            
+                                <Loader size='large' active>Loading</Loader>
+                         
+                        </Grid.Column>
+                 
+                </Grid>
+                :
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <PageHeader user={user} handleLogout={handleLogout}/>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <ProfileBio user={profileUser} />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row centered>
+                        <Grid.Column style={{ maxWidth: 750 }}>
+                            <PostFeed isProfile={true} posts={posts} numPhotosCol={3} user={user} addLike={addLike} removeLike={removeLike}/>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            }
+        </>
+    )
 }

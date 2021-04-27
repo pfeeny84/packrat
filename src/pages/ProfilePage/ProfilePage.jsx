ad9bd90c695c345e689cb39b8fb63d9e28e4f355
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-04-27 10:29:07 -0700

    rendering progile page w/ components

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Segment, Dimmer, Loader } from 'semantic-ui-react'
-import userService from '../../utils/userService';
+import {  Grid } from 'semantic-ui-react'
+import ProfileBio from '../../components/ProfileBio/ProfileBio';
+import ProfilePostDisplay from '../../components/ProfilePostDisplay/ProfilePostDisplay';
 import PageHeader from '../../components/Header/Header';
 
 
@@ -16,6 +17,16 @@ export default function ProfilePage({ user, handleLogout }){
                 <PageHeader user={user} handleLogout={handleLogout}/>
             </Grid.Column>
         </Grid.Row>
+        <Grid.Row>
+            <Grid.Column>
+                <ProfileBio />
+            </Grid.Column>
+        </Grid.Row>
+        <Grid.Row centered>
+            <Grid.Column style={{ maxWidth: 750 }}>
+                <ProfilePostDisplay />
+            </Grid.Column>
+            </Grid.Row>
         </Grid>
           
              <h1>This is the Profile Page</h1>

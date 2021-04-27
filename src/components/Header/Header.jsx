433e898a07a06548b365b6ff0c92c27568c29437
pfeeny84 <77941384+pfeeny84@users.noreply.github.com>
2021-04-26 17:10:11 -0700

    styling header and linking the main logo to link to home page.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,14 +8,14 @@ export default function PageHeader({user, handleLogout}){
     return (
         <Segment clearing inverted >
             <Header as='h2' floated='right'>
-                <Link to="/"><FaGuitar color='white' /></Link>
+                <Link to="/"><FaGuitar color='red' /></Link>
                 <Link style={{color: 'red'}} to='' onClick={handleLogout} >Logout</Link>
             </Header>
             <Header  as='h2' floated='left'>
                 <Link style={{color: 'red'}} to={`/${user.username}`}><Image src={user.photoUrl ? user.photoUrl : "https://react.semantic-ui.com/images/wireframe/square-image.png"} avatar></Image>{ user.username.toUpperCase() }</Link>          
             </Header>
-            <Header as='h1' textAlign='center' color='red' dividing>
-                PackRat
+            <Header  as='h1' textAlign='center' color='red' dividing>
+                <a href='/'><span className='header-text'>PackRat</span></a>
             </Header>
         </Segment>
     )
